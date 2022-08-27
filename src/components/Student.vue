5c28a52d384e9f18ffad9ffc251bfff04a44c899
Sergio Díaz <codediaz@outlook.es>
2022-08-27 15:06:20 -0500

    feature: request with axios

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -7,10 +7,11 @@
         <h1>Sistema de titulación </h1> 
 
         <div class="row">
-          <input type="text" v-model="buscar" class="form-control" placeholder="Ejemplo: 0927020495"/>
+          <input type="text" v-model="student_code" class="form-control" placeholder="Ejemplo: 0927020495"/>
+          <span>{{ error  }}</span>
         </div>      
         <div class="row" style="float:right">
-           <button type="button" class="btn btn-secondary btn-lg mt-1" function="searchStudent()">Buscar</button>   
+           <button type="button"  class="btn btn-secondary btn-lg mt-1" v-on:click ="searchStudents()">Buscar</button>   
         </div>  
                      
  
@@ -34,19 +35,19 @@
      Nombre:
     </div>
     <div class="col-sm">
-      <input type="text" class="form-control" disabled />
+      <input type="text" class="form-control" :value="name" disabled />
     </div>
     <div class="col-sm">
       Apellido:
     </div>
     <div class="col-sm">
-     <input type="text" class="form-control" disabled/>
+     <input type="text" class="form-control" v-model="lastname" disabled/>
     </div>
     <div class="col-sm">
       Titulación:
     </div>
     <div class="col-sm">
-     <input type="text" class="form-control" disabled />
+     <input type="text" class="form-control" v-model="topic" disabled />
     </div>
   </div>
 </div>
@@ -95,33 +96,69 @@
 
 </div>
 
-
-
-
 </template>
 
 
 <script>
+import axios from 'axios'
 export default{
   data(){
     return{
-      students:[]
+      students:{},
+      student_code:'',
+      name: '',
+      lastname: '',
+      topic:'',
+      error: ''
     }
   },
 
   methods:{
-    searchStudent(){
-      fetch('http://localhost/students/{student}')
-      .then(response=>response.json())
-      .then((dataResponse)=>{
-        console.log(dataResponse)
-        this.students=[]
-        if(typeof dataResponse[0].succes== "undefined"){
-          this.students = dataResponse
-        }
-      })
-      .catch(console.log)
-    }
+     async searchStudents(){
+
+      try{
+        const r = await axios.get(` http://localhost/v1/students/${this.student_code}`)
+      
+
+        this.name = r.data.first_name;
+        this.lastname = r.data.surname;
+        this.topic = r.data.topic;
+        this.error = '';
+
+      }
+      catch(e){
+        this.error = 'Estudiante no encontrado'
+        this.name = '';
+        this.lastname = '';
+        this.topic = '';
+      }
+      
+    },
+
+    /*async Phase1(){
+      try {
+        const r = await axios.post()
+        
+      } catch (error) {
+        
+      }
+    },
+    async Phase2(){
+      try {
+        const r = await axios.post()
+        
+      } catch (error) {
+        
+      }
+    },
+    async Phase3(){
+      try {
+        const r = await axios.post()
+        
+      } catch (error) {
+        
+      }
+    }*/
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(axios)
 new Vue({
   router,
   render: h => h(App)
